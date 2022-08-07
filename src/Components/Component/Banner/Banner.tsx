de40019a37652d27b1e0f4f25ac3d08614a85758
masudRana88 <mahinuddin88@gmail.com>
2022-08-07 15:19:14 +0600

    [banner bg added]

--- src/Components/Component/Banner/Banner.tsx
+++ src/Components/Component/Banner/Banner.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import chair from "./../../../images/images/chair.png"
 const Banner = () => {
     return (
-        <section className=' w-full px-4 h-[70vh] bg-slate-700 mt-[66px]'>
+        <section className=' w-full px-4 h-full md:h-[94vh] bg-bannerBG mt-[66px]'>
             <div className='items-center justify-center h-full gap-9 md:flex md:container md:mx-auto md:flex-row-reverse'>
         
                 {/* row 1 */}
@@ -15,7 +15,7 @@ const Banner = () => {
                 </div>
                 
                 {/* row 2 */}
-                <div className='w-full mt-10 md:w-2/4 md:mt-0'>
+                <div className='w-full pb-10 mt-10 md:w-2/4 md:mt-0'>
                     <h2 className='mb-3 text-4xl font-bold text-textColorH'>Your New Smile Starts Here</h2>
                     <p className='mb-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
                     <button className='px-4 py-2.5 rounded-md font-semibold text-white bg-gradient-to-bl from-bgGreenR to-bgGreenL'>GET STARTED</button>
